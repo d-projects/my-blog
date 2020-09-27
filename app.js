89263d42bc18f436ad8676fbb8b922ea74414e46
d-projects <60767346+d-projects@users.noreply.github.com>
2020-09-26 23:12:36 -0700

    Add databse functionality

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+const config = require('./settings/config.js');
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
+const moment = require('moment');
+
+// set up express and view engine (ejs)
 const app = express();
-
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+// connects to MongoDB
+const dbURI = `mongodb+srv://${config.db_user}:${config.db_password}@${config.db_cluster}.ffgxc.mongodb.net/${config.db_name}`;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('connected');
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err);
+    });
 
+    console.log(dbURI)
 
-// URL Routes
+// URL Non-Blog Routes
 app.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
@@ -19,6 +33,44 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.render('contact');
+});
+
+app.get('/admin', (req, res) => {
+    if (req.query.message){
+        res.render('admin', { message: req.query.message });
+    } else {
+        res.render('admin');
+    }
+    
+});
+
+
+// URL Blog Specific Routes
+app.get('/blogs', (req, res) => {
+    Blog.find()
+    .then (result => {
+        res.render('index', {blogs: result, moment: moment});
+    })
+    .catch (err => {
+        res.send(err);
+    });
+    
+});
+
+// POST Routes
+app.post('/admin', (req, res) => {
+    const blogData = req.body;
+    const blog = new Blog({
+        title: blogData.title,
+        body: blogData.body
+    });
+    blog.save()
+    .then ( result => {
+        res.redirect('/admin?message=Success!');
+    })
+    .catch ( err => {
+        console.log(err);
+    });
 });
 
 app.use((req, res) => {
---
+++ settings/config.js
@@ -0,0 +1,8 @@
+const config = {
+    db_user: 'fake-username',
+    db_password: 'fake-password',
+    db_cluster: 'fake-cluster-name',
+    db_name: 'fake-database-name'
+}
+
+module.exports = config;
