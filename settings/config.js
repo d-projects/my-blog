a6a3d652e1c0de20fbd46d0db5b1caf5ea53bf2d
d-projects <60767346+d-projects@users.noreply.github.com>
2020-10-12 19:53:08 -0700

    Remove ignore config.js

---
+++ settings/config.js
@@ -0,0 +1,15 @@
+/*
+ * Note that this file is a template
+ */
+
+const config = {
+    db_user: 'dulhan',
+    db_password: 'cbtjTNU626tSBkGV',
+    db_cluster: 'blogapp',
+    db_name: 'blog-db',
+    session_secret: '(-t>2RNM}JXYBTs~',
+    email: 'dulhanh@gmail.com',
+    password: 'halifax1234'
+}
+
+module.exports = config;
